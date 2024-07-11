7126b60a59c64a36d7bcebe2a7c977ac4340f07f
Gianpietro Di Matteo <gianpietro.dimatteo@gmail.com>
2024-07-11 16:18:18 -0300

    next configuration for static websites

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/taskmate",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
